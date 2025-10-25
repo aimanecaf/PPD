e9b4368637863db3e47543f7edcad8ca2c31aa5b
aimanecaf <aimanecaf@gmail.com>
2025-10-25 12:02:50 +0200

    Initial commit with Git LFS

--- app.py
+++ app.py
@@ -35,10 +35,7 @@ app.layout = html.Div([
     dash.page_container,
     footer,  # Include the footer
 ])
-
+server = app.server
 # Run the dash app
-#if __name__ == '__main__':
-#    app.run(debug=True)
-
-if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
